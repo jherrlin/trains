f31ef6ea5625b14d168121fe54d40efcd66de85e
Robin Karlsson <rk222ev@student.lnu.se>
2016-01-23 13:04:01 +0100

    Introduce test transit route

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,10 +10,33 @@
 angular.module('projectsApp')
   .controller('MainCtrl', function () {
 
+    var route = {
+      origin: "Alvesta",
+      destination: " Växjö",
+      provideRouteAlternatives: false,
+      travelMode: google.maps.TravelMode.TRANSIT,
+      drivingOptions: {
+        departureTime: new Date(/* now, or future date */),
+        trafficModel: google.maps.TrafficModel.PESSIMISTIC
+      }
+  }
+
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+
     // Initialize map
       var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 56.8665586, lng: 14.7000050},
         zoom: 9
+      });
+
+      directionsDisplay = new google.maps.DirectionsRenderer();
+      directionsDisplay.setMap(map);
+
+      directionsService.route(route, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(result);
+        }
       });
 
     this.awesomeThings = [
