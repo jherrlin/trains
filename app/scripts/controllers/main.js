89a078e6ced4c8b0aa63950af8a6d8c501bd9e76
Robin Karlsson <rk222ev@student.lnu.se>
2016-01-23 12:40:58 +0100

    Introduce google maps

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,13 @@
  */
 angular.module('projectsApp')
   .controller('MainCtrl', function () {
+
+    // Initialize map
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+      });
+
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
