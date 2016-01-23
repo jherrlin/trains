44eeb0050a06f9542159fe6ff202f6a8dac5c006
Robin Karlsson <rk222ev@student.lnu.se>
2016-01-23 20:36:10 +0100

    Move route logic to factory

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,37 +8,31 @@
  * Controller of the projectsApp
  */
 angular.module('projectsApp')
-  .controller('MainCtrl', function ($scope, stations, carDistance) {
+  .controller('MainCtrl', function ($scope, carDistance, route, stations)  {
 
     var directionsService = new google.maps.DirectionsService();
     var directionsDisplay = new google.maps.DirectionsRenderer();
-
     var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 56.8665586, lng: 14.7000050},
-    zoom: 9
+      center: {lat: 56.8665586, lng: 14.7000050},
+      zoom: 9
     });
-
 
     $scope.stations = stations;
     $scope.start = null;
     $scope.destination = null;
 
     $scope.changeTrip = function () {
+      var start;
+      var destination;
+
       if ($scope.start === null || $scope.destination === null) { return; };
 
-      var route = {
-        origin: stations[$scope.start].name,
-        destination: stations[$scope.destination].name,
-        provideRouteAlternatives: false,
-        travelMode: google.maps.TravelMode.TRANSIT,
-        transitOptions: {
-          modes: [ google.maps.TransitMode.RAIL]
-        }
-      };
+      start = stations[$scope.start].name;
+      destination = stations[$scope.destination].name
 
       directionsDisplay.setMap(map);
 
-      directionsService.route(route, function(result, status) {
+      directionsService.route(route.create(start, destination), function(result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
           directionsDisplay.setDirections(result);
         }
---
+++ app/scripts/services/route.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name projectsApp.route
+ * @description
+ * # route
+ * Factory in the projectsApp.
+ */
+angular.module('projectsApp')
+  .factory('route', function () {
+    var route = {
+      provideRouteAlternatives: false,
+      travelMode: google.maps.TravelMode.TRANSIT,
+      transitOptions: {
+        modes: [ google.maps.TransitMode.RAIL]
+      }
+    };
+
+    // Public API here
+    return {
+      create: function (start, destination) {
+        route.origin = start;
+        route.destination = destination;
+        return route;
+      }
+    };
+  });
