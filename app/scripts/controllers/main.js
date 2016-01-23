f74a29e9bbd7bc6e3544c133a3c28360b758d867
Robin Karlsson <rk222ev@student.lnu.se>
2016-01-23 14:30:48 +0100

    Choose only trains

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,39 +9,30 @@
  */
 angular.module('projectsApp')
   .controller('MainCtrl', function (stations) {
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
 
     var route = {
       origin: "Alvesta",
       destination: " Växjö",
       provideRouteAlternatives: false,
       travelMode: google.maps.TravelMode.TRANSIT,
-      drivingOptions: {
-        departureTime: new Date(/* now, or future date */),
-        trafficModel: google.maps.TrafficModel.PESSIMISTIC
+      transitOptions: {
+        modes: [ google.maps.TransitMode.RAIL]
       }
     };
 
-    var directionsDisplay;
-    var directionsService = new google.maps.DirectionsService();
-
-    // Initialize map
     var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 56.8665586, lng: 14.7000050},
-      zoom: 9
-      });
+    center: {lat: 56.8665586, lng: 14.7000050},
+    zoom: 9
+    });
 
-      directionsDisplay = new google.maps.DirectionsRenderer();
-      directionsDisplay.setMap(map);
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setMap(map);
 
-      directionsService.route(route, function(result, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-          directionsDisplay.setDirections(result);
-        }
-      });
-
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    directionsService.route(route, function(result, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(result);
+      }
+    });
   });
