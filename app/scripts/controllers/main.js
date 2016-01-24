58d3afb081dd0889fb5d899ab4a1212458fec359
Robin Karlsson <rk222ev@student.lnu.se>
2016-01-24 16:08:12 +0100

    Add a default start and destination

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,50 +22,48 @@ angular.module('projectsApp')
       zoom: 9
     });
 
-    $scope.stations = stations;
-    $scope.start = null;
-    $scope.destination = null;
-    $scope.gasconsuming = 0.7;
-    $scope.gasprice = 18;
-    $scope.gasconsuming_total = 0;
-
-
-    $scope.traindistance = null;
-    $scope.cardistance = null;
-    $scope.carprice =  null;
-
-
-
     $scope.changeTrip = function () {
-      var start;
+      var carRoute;
       var destination;
+      var start;
+      var trainRoute;
 
       if ($scope.start === null || $scope.destination === null) { return; }
-
-      start = stations[$scope.start];
-      destination = stations[$scope.destination];
 
       trainDisplay.setMap(map);
       carDisplay.setMap(map);
 
-      directionsService.route(route.traincreate(start.lat, start.lng, destination.lat, destination.lng), function(result, status) {
+      start = stations[$scope.start];
+      destination = stations[$scope.destination];
+
+      trainRoute = route.traincreate(start.lat, start.lng, destination.lat, destination.lng);
+      carRoute = route.carcreate(start.lat, start.lng, destination.lat, destination.lng);
+
+      directionsService.route(trainRoute, function(result, status) {
         if (status === google.maps.DirectionsStatus.OK) {
           $scope.traindistance = result.routes[0].legs[0].distance;
           trainDisplay.setDirections(result);
+          $scope.$apply(); // Force rerendering of UI
         }
       });
 
-      directionsService.route(route.carcreate(start.lat, start.lng, destination.lat, destination.lng), function(result, status) {
+      directionsService.route(carRoute, function(result, status) {
         if (status === google.maps.DirectionsStatus.OK) {
           $scope.cardistance = result.routes[0].legs[0].distance;
           $scope.carprice = ((((result.routes[0].legs[0].distance.value/10000.0*$scope.gasprice)*$scope.gasconsuming)*40)*2); // Think that this cals are wrong ;)
           $scope.gasconsuming_total = ((((result.routes[0].legs[0].distance.value/10000.0)*$scope.gasconsuming)*40)*2);  // Think that this cals are wrong ;)
           carDisplay.setDirections(result);
+          $scope.$apply(); // Force rerendering of UI
         }
       });
-
-
-
     };
 
+    $scope.stations = stations;
+    $scope.start = 0;
+    $scope.destination = 7;
+    $scope.gasconsuming = 0.7;
+    $scope.gasprice = 18;
+    $scope.gasconsuming_total = 0;
+
+    $scope.changeTrip();
   });
